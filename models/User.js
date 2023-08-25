22fbe465e50f2fdb1dd227a84acc7885f809e5ae
shashank <shashankofficial3209@gmail.com>
2023-08-25 23:50:10 +0530

    Userl-Model-createdl

---
+++ models/User.js
@@ -0,0 +1,17 @@
+
+const { DataTypes } = require('sequelize');
+const sequelize = require('../db');
+
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: DataTypes.STRING,
+  email: DataTypes.STRING,
+});
+
+module.exports = User;
+
+
